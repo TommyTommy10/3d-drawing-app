8f44e39e2d5739ecba47d74ec341ff1a57a25686
TommyTommy10 <tommaso.grade@gmail.com>
2024-09-30 18:19:30 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,18 @@
+import React from 'react';
+import Canvas from './components/Canvas';
+import Toolbar from './components/Toolbar';
+import Sidebar from './components/Sidebar';
 
+function App() {
+  return (
+    <div className="App">
+      <Toolbar />
+      <div className="main-content">
+        <Sidebar />
+        <Canvas />
+      </div>
+    </div>
+  );
+}
+
+export default App;
