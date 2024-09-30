e059b8bd21a570ec077d568fa46572e36ee78a20
TommyTommy10 <tommaso.grade@gmail.com>
2024-09-30 18:20:15 +0200

    Update Toolbar.js

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -1,1 +1,13 @@
+import React from 'react';
 
+const Toolbar = () => {
+  return (
+    <div className="toolbar">
+      <button>Strumento 1</button>
+      <button>Strumento 2</button>
+      <button>Strumento 3</button>
+    </div>
+  );
+};
+
+export default Toolbar;
