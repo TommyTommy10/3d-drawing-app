bd3e287fd3c45526db9d192e2b632e1691830ed9
TommyTommy10 <tommaso.grade@gmail.com>
2024-09-30 18:20:33 +0200

    Update Sidebar.js

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,1 +1,16 @@
+import React from 'react';
 
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <h2>Opzioni</h2>
+      <ul>
+        <li>Opzione 1</li>
+        <li>Opzione 2</li>
+        <li>Opzione 3</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
